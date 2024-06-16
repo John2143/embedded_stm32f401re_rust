19670f8199e15cf19a3ebb713fbd7d8df47b3645
John Schmidt <john@john2143.com>
2024-06-16 00:47:09 -0400

    compile

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+#![no_std]
+#![no_main]
+
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use embassy_stm32::gpio::{AnyPin, Input, Level, Output, Speed, Pin, Pull};
 use embassy_stm32::Peripherals;
+
+use panic_probe as _;
+use defmt_rtt as _;
 
 // Declare async tasks
 #[embassy_executor::task]
@@ -19,7 +25,7 @@ async fn blink(pin: AnyPin) {
 }
 
 // Main is itself an async task as well.
-#[entry]
+#[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
@@ -27,13 +33,13 @@ async fn main(spawner: Spawner) {
     spawner.spawn(blink(p.PA5.degrade())).unwrap();
     //spawner.spawn(blink(p.PB13.degrade())).unwrap();
 
-    let mut button = Input::new(p.PC13, Pull::Up);
-    loop {
-        // Asynchronously wait for GPIO events, allowing other tasks
-        // to run, or the core to sleep.
-        button.wait_for_low().await;
-        info!("Button pressed!");
-        button.wait_for_high().await;
-        info!("Button released!");
-    }
+    // let mut button = Input::new(p.PC13, Pull::Up);
+    // loop {
+    //     // Asynchronously wait for GPIO events, allowing other tasks
+    //     // to run, or the core to sleep.
+    //     button.wait_for_low().await;
+    //     info!("Button pressed!");
+    //     button.wait_for_high().await;
+    //     info!("Button released!");
+    // }
 }
