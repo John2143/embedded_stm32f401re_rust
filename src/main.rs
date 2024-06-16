868b869da0e9751cef5f652f1a7f63d0df89870f
John Schmidt <john@john2143.com>
2024-06-16 15:35:01 -0400

    working i2c

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,17 @@
 
 use defmt::println;
 use embassy_executor::Spawner;
-use embassy_stm32::{gpio::{AnyPin, Level, Output, Pin, Pull, Speed}};
+use embassy_stm32::{
+    bind_interrupts, dma::NoDma, gpio::{AnyPin, Level, Output, Pin, Pull, Speed}, i2c, pac::i2c::I2c, peripherals, time::Hertz, usart::{self, UartTx}
+};
 use embassy_sync::{blocking_mutex::raw::NoopRawMutex, signal::Signal};
 use embassy_time::Timer;
 
-use core::{borrow::BorrowMut, cell::{OnceCell, RefCell}, sync::atomic::{AtomicU32, Ordering}};
+use core::{
+    borrow::BorrowMut,
+    cell::{OnceCell, RefCell},
+    sync::atomic::{AtomicU32, Ordering},
+};
 
 use defmt_rtt as _;
 use panic_probe as _;
@@ -31,10 +37,24 @@ async fn blink(pin: AnyPin) {
 static SIGNAL_A: AtomicU32 = AtomicU32::new(100);
 static SIGNAL_B: AtomicU32 = AtomicU32::new(900);
 
+bind_interrupts!(struct Irqs {
+    //USB => usb::InterruptHandler<peripherals::USB>;
+    I2C1_EV => i2c::EventInterruptHandler<peripherals::I2C1>;
+    I2C1_ER => i2c::ErrorInterruptHandler<peripherals::I2C1>;
+});
+
 // Main is itself an async task as well.
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
+
+    let spd = Hertz::hz(115200);
+    let mut cfg = embassy_stm32::i2c::Config::default();
+    cfg.sda_pullup = true;
+    cfg.scl_pullup = true;
+    let mut i2c = embassy_stm32::i2c::I2c::new(p.I2C1, p.PB8, p.PB9, Irqs, p.DMA1_CH6, p.DMA1_CH1, spd, cfg);
+    let x = i2c.write(0x80, b"yey").await;
+    println!("I2C result {:?}", x);
 
     println!("Starting blinking program");
     SIGNAL_A.store(100, Ordering::SeqCst);
@@ -42,9 +62,12 @@ async fn main(spawner: Spawner) {
 
     let button = embassy_stm32::gpio::Input::new(p.PC13, Pull::Up);
     loop {
+        //let _ = usart.blocking_write(b"Hello World\n").unwrap();
+        //Timer::after_millis(10).await;
         while button.is_high() {
             Timer::after_millis(10).await;
         }
+
         println!("Button pressed!");
         SIGNAL_A.store(50, Ordering::SeqCst);
         SIGNAL_B.store(50, Ordering::SeqCst);
